81022259d8dff320b4afbead89234042f68fca68
camilvs <urenacamila0@gmail.com>
2025-04-19 13:21:55 -0400

    Vendor- add products functionality
    vendor/products/add_product
    -needs to be styled better

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySuperSecretKey1234567890'
 
 # *** Connect Database ***
-conn_str = "mysql+pymysql://root:CSET115@localhost/egardens"
+conn_str = "mysql+pymysql://root:CSET155@localhost/egarden"
 engine = create_engine(conn_str, echo=True)
 
 @app.route('/')
@@ -83,7 +83,37 @@ def vendor():
 
 @app.route('/vendor/products')
 def manage_products():
+
     return render_template('manage_products.html')
+
+@app.route('/vendor/products/add_product', methods=["GET",'POST'])
+def add_product():
+    # if 'username' not in session:
+    #     return redirect(url_for('login'))
+    if request.method == "POST":
+        product_name = request.form['product_name']
+        product_desc = request.form['product_desc']
+        product_color = request.form['product_color']
+        product_sizes = request.form['product_sizes']
+        product_quantity= request.form['product_quantity']
+        original_price = request.form['original_price']
+        discount_price= request.form['discount_price']
+
+        with engine.begin() as conn:
+            conn.execute(text('INSERT INTO products (product_name, product_desc, product_color, product_sizes, product_quantity, original_price, discount_price) VALUES (:product_name, :product_desc, :product_color, :product_sizes, :product_quantity, :original_price, :discount_price)'), {
+                'product_name': product_name,
+                'product_desc': product_desc,
+                'product_color': product_color,
+                'product_sizes': product_sizes,
+                'product_quantity':product_quantity,
+                'original_price': original_price,
+                'discount_price': discount_price
+            })
+
+            msg = 'You have successfully added a product.'
+            return render_template('add_product.html',msg=msg)
+    return render_template('add_product.html')
+
 
 @app.route('/vendor/prices')
 def update_prices():
